7911ee78f97585c0387ff1fd3b3d58271c262692
Asmaa Ahmed Aswefy <aswefey@meemdev.com>
2024-07-15 10:08:29 +0300

    logging service

---
+++ src/app/services/logging.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggingService {
+
+  constructor() { }
+
+  /**
+   * Logs a message to the console with a prefix indicating that it is an API response.
+   *
+   * @param {string} message - The message to be logged.
+   */
+  simplelog(message: string) {
+    console.log((`Api Response is - x ${message}`), message);
+  }
+
+  formatedMessageLog(message: string): void {
+    console.log('%cApi Response is - ${message}', 'background: blue; color: yellow; font-size: 14px;', message);
+  }
+}
